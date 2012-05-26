e045e79b89de20ec35cfaf6e388f78c6c7716796
naosim <nao_pillows@yahoo.co.jp>
2012-05-26 15:28:24 +0900

    ファイルアクセス系のメソッド追加

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,7 @@ var clickLaunchButton = function() {
 	
 	var path = getPath(this.parentElement);
 	air.trace("path: " + path);
+	launch();
 	
 };
 
@@ -15,14 +16,27 @@ var clickLaunchButton = function() {
  * ファイルパスを取得する
  * @param liElm liタグの要素
  */
-
 var getPath = function(liElm) {
 	air.trace("liElm: " + liElm);
 	return liElm.getElementsByClassName('pathBox')[0].value;
 };
 
 var launch = function(path) {
-	
+var nativeProcessStartupInfo = new air.NativeProcessStartupInfo(); 
+var file = air.File.applicationDirectory.resolvePath("test.exe"); 
+nativeProcessStartupInfo.executable = file; 
+var processArgs = new air.Vector["<String>"](); 
+processArgs.push("hello"); 
+nativeProcessStartupInfo.arguments = processArgs; 
+process = new air.NativeProcess();
+var onOutputData = function(event) { 
+    var stdOut = process.standardOutput; 
+    var data = stdOut.readUTFBytes(process.standardOutput.bytesAvailable); 
+    air.trace("Got: ", data); 
+};
+process.addEventListener(air.ProgressEvent.STANDARD_OUTPUT_DATA, onOutputData); 
+process.start(nativeProcessStartupInfo); 
+
 };
 
 window.onload = function() {
