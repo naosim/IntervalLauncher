d44416a5cc5eed0903f2127265528f69e42f2f7c
naosim <nao_pillows@yahoo.co.jp>
2012-05-26 20:21:36 +0900

    保存機能追加

--- src/js/main.js
+++ src/js/main.js
@@ -16,44 +16,63 @@ var getPath = function(liElm) {
 	return liElm.getElementsByClassName('pathBox')[0].value.replace(/^\s+|\s+$/g, "");
 };
 
+var getLocalData = function() {
+	return air.SharedObject.getLocal("data").data.data;
+};
+
+var setLocalData = function(json) {
+	var so = air.SharedObject.getLocal("data");
+	so.data.data = json;
+	so.flush();
+};
+
 var launch = function(path) {
-	/*
-	if(!air.NativeProcess.isSupported) {
-		air.trace("bat bat");
-		alert("batbat");
-	
-		return;
-	} else {
-		air.trace("good good");
-		alert("goodgood");
-	}
-	*/
-	
 	air.trace("path: " + path);
 	
 	var nativeProcessStartupInfo = new air.NativeProcessStartupInfo(); 
 	var file = new air.File(path);
 	file.openWithDefaultApplication();
-	// var file = air.File.applicationDirectory.resolvePath("/Users/nao_pillows/Documents/Aptana Studio 3 Workspace/IntervalLauncher/src/hoge.txt"); 
-	// nativeProcessStartupInfo.executable = file; 
-// var processArgs = new air.Vector["<String>"](); 
-// processArgs.push("hello"); 
-// nativeProcessStartupInfo.arguments = processArgs; 
-	// var process = new air.NativeProcess();
-// var onOutputData = function(event) { 
-    // var stdOut = process.standardOutput; 
-    // var data = stdOut.readUTFBytes(process.standardOutput.bytesAvailable); 
-    // air.trace("Got: ", data);
-// };
-// process.addEventListener(air.ProgressEvent.STANDARD_OUTPUT_DATA, onOutputData); 
-	// process.start(nativeProcessStartupInfo); 
-
 };
 
 window.onload = function() {
+	// ボタンの設定
 	var launchButtons = document.getElementsByClassName('launchButton');
 	for(var i = 0; i < launchButtons.length; i++) {
 		var button = launchButtons[i];
 		button.addEventListener('click', clickLaunchButton);
+	}
+	
+	// 入力エリアの設定
+	var data = getLocalData();
+	if(!data) {
+		air.trace("data is null");
+		setLocalData(["","","","",""]);
+		return;
+	}
+	else {
+		air.trace("data exist");
+	}
+	var boxes = document.getElementsByClassName('pathBox');
+	for(var i = 0; i < boxes.length; i++) {
+			var box = boxes[i];
+			box.value = data[i];
+	}
+	
+	// 保存ボタン
+	var saveButton = document.getElementsByClassName('saveButton')[0];
+	saveButton.addEventListener('click', clickSave);
+};
+
+/**
+ * 保存
+ */
+var clickSave = function() {
+	air.trace("clickSave");
+	var boxes = document.getElementsByClassName('pathBox');
+	var data = ["","","","",""];
+	for(var i = 0; i < boxes.length; i++) {
+			var box = boxes[i];
+			data[i] = box.value;
+			setLocalData(data);
 	}
 };
